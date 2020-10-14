1e60fa66d57f359d6264d5a0e2cb112f51b4613d
Kris Chavez <chavez.kris3@gmail.com>
2020-10-14 19:57:55 +1100

    Added SOUNDS

--- app/src/main/java/com/example/soundpool/MainActivity.java
+++ app/src/main/java/com/example/soundpool/MainActivity.java
@@ -21,8 +21,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private SoundPool soundPool;
-    private int sound1, sound2, sound3, sound4, sound5, sound6, sound7;
-    private int sound3StreamId;
+    private int sound1, sound2, sound3, sound4, sound5, sound6;
 
     private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
     //views from activity
@@ -104,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                     .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
                     .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                     .build();
+
             soundPool = new SoundPool.Builder()
                     .setMaxStreams(6)
                     .setAudioAttributes(audioAttributes)
@@ -112,33 +112,37 @@ public class MainActivity extends AppCompatActivity {
             soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
         }
 
-        sound1 = soundPool.load(this, R.raw.sound1, 1);
-        sound2 = soundPool.load(this, R.raw.sound2, 1);
-        sound3 = soundPool.load(this, R.raw.sound3, 1);
-        sound4 = soundPool.load(this, R.raw.sound4, 1);
+        sound1 = soundPool.load(this, R.raw.goat, 1);
+        sound2 = soundPool.load(this, R.raw.dog, 1);
+        sound3 = soundPool.load(this, R.raw.cat, 1);
+        sound4 = soundPool.load(this, R.raw.cow, 1);
+        sound5 = soundPool.load(this, R.raw.horse, 1);
+        sound6 = soundPool.load(this, R.raw.lion, 1);
+
     }
     public void playSound(View v) {
         switch (v.getId()) {
             case R.id.button_sound1:
                 soundPool.play(sound1, 1, 1, 0, 0, 1);
-                soundPool.autoPause();
                 break;
            case R.id.button_sound2:
                 soundPool.play(sound2, 1, 1, 0, 0, 1);
                 break;
             case R.id.button_sound3:
-                sound3StreamId = soundPool.play(sound3, 1, 1, 0, 0, 1);
+                soundPool.play(sound3, 1, 1, 0, 0, 1);
                 break;
             case R.id.button_sound4:
                 soundPool.play(sound4, 1, 1, 0, 0, 1);
                 break;
-            case R.id.button_sound5: soundPool.play(sound5, 1, 1, 0, 0, 1);
+            case R.id.button_sound5:
+                soundPool.play(sound5, 1, 1, 0, 0, 1);
                 break;
             case R.id.button_sound6:
                 soundPool.play(sound6, 1, 1, 0, 0, 1);
                 break;
         }
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
