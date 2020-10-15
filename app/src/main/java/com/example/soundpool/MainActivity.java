5da31490102e714ffe3111e0ee7f880d9086a1f9
mailongd <longm4020@gmail.com>
2020-10-16 10:01:43 +1100

    Added SOUNDS

--- app/src/main/java/com/example/soundpool/MainActivity.java
+++ app/src/main/java/com/example/soundpool/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.soundpool;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
